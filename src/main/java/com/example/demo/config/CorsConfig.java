1d9c89947910fd861e09c764c2b401d60c8f9a88
Sanghoon Lee <gcccompil3r@gmail.com>
2023-04-28 18:38:52 +0900

    feat: Cors Config [BDT]

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:8081")
+                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://15.164.174.190")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
